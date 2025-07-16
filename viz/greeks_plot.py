92baab998621bc39ff2aceae35b208994e353446
Benjamin Duncanson <>
2025-07-16 18:25:07 +1000

    plotting greeks vs spot price in matplotlib

--- viz/greeks_plot.py
+++ viz/greeks_plot.py
@@ -1,3 +1,20 @@
-def plot_greeks_vs_price(S_range, K, T, r, sigma):
-    # generates a subplot of each greek
-    ...
+import numpy as np
+import matplotlib.pyplot as plt
+from core.pricing import greeks
+
+def plot_greek_vs_spot(K, T, r, sigma, greek='delta', option_type='call'):
+    S = np.linspace(0.5*K, 1.5*K, 100)
+    values = []
+
+    for s in S:
+        g = greeks(s, K, T, r, sigma, option_type)
+        values.append(g[greek])
+
+    plt.figure(figsize=(8, 5))
+    plt.plot(S, values, label=f'{greek.capitalize()} ({option_type})')
+    plt.xlabel('Spot Price')
+    plt.ylabel(greek.capitalize())
+    plt.title(f'{greek.capitalize()} vs Spot Price')
+    plt.grid(True)
+    plt.legend()
+    plt.show()
